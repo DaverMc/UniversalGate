1fb71eb0610cd98984243f6afd8804d2a1cdbf6d
Daver <dennis.fleer@outlook.de>
2026-01-22 01:59:05 +0100

    Started working on Item rename/set lore

--- src/main/java/de/daver/unigate/statue/StatueSettingsDialog.java
+++ src/main/java/de/daver/unigate/statue/StatueSettingsDialog.java
@@ -20,7 +20,7 @@ public class StatueSettingsDialog {
         var lang = plugin.languageManager();
         var attributes = statue.attributes();
         var dialogBase = dialogBase(lang, player, attributes);
-        var dialogType = DialogType.confirmation(confirmChangesButton(lang, player), deleteStatueButton(lang, player));
+        var dialogType = DialogType.multiAction(List.of(confirmChangesButton(lang, player), deleteStatueButton(lang, player))).build();
 
         return Dialog.create(builder -> builder.empty()
                 .base(dialogBase)
